53035c437762c8c14a9eaf04e4147cef2f70e5b0
Márcio Araújo <marcioxmarcio@gmail.com>
2024-06-05 16:04:21 -0300

    Update import_from_file.py

--- import_from_file.py
+++ import_from_file.py
@@ -1,68 +1,69 @@
-#!/anatel/env/bin/python3
+#!/usr/bin/python3
 
+import sys
 import PyPDF2
 import re
-import sys
 from datetime import datetime
 
 def extrair_texto_de_pdf(caminho_do_arquivo):
-    # Abrir o arquivo PDF em modo leitura binária
-    with open(caminho_do_arquivo, 'rb') as arquivo:
-        leitor_pdf = PyPDF2.PdfReader(arquivo)
-        texto_completo = []
-
-        # Iterar sobre cada página no arquivo PDF
-        for pagina in leitor_pdf.pages:
-            # Extrair texto da página atual
-            texto_pagina = pagina.extract_text()
-            if texto_pagina:
-                texto_completo.append(texto_pagina)
-
-        # Unir todos os textos extraídos em uma única string
-        return "\n".join(texto_completo)
+    try:
+        with open(caminho_do_arquivo, 'rb') as arquivo:
+            leitor_pdf = PyPDF2.PdfReader(arquivo)
+            texto_completo = []
+            for pagina in leitor_pdf.pages:
+                texto_pagina = pagina.extract_text()
+                if texto_pagina:
+                    texto_completo.append(texto_pagina)
+            return "\n".join(texto_completo)
+    except Exception as e:
+        print(f"Erro ao extrair texto do PDF: {e}")
+        sys.exit(1)
 
 def gerar_entradas_dns(url):
-    # Extrair o domínio principal da URL
     dominio = re.findall(r"\b(?:http(?:s)?://)?(\w+[\w\-\.]+(?:\.\w+)+)\b", url)
     if dominio:
-        # Retorna entradas CNAME para o domínio e para o wildcard '*'
         return f"{dominio[0]} IN CNAME .\n*{dominio[0]} IN CNAME ."
     return ""
 
-def gerar_cabecalho_dns():
-    # Gerar serial com base na data e hora atual
-    data_atual = datetime.now()
-    serial = data_atual.strftime("%Y%m%d%H")
-    cabecalho = f"""$TTL 1H
-@       IN      SOA LOCALHOST. localhost.localhost. (
-                {serial}      ; Serial
-                1h            ; Refresh
-                15m           ; Retry
-                30d           ; Expire
-                2h            ; Negative Cache TTL
-        )
-        NS  localhost."""
-    return cabecalho
+def imprimir_cabecalho_dns():
+    serial = datetime.now().strftime("%Y%m%d%H")
+    header = f"$TTL 1H\n@       IN      SOA LOCALHOST. localhost.localhost. (\n                {serial}      ; Serial\n                1h              ; Refresh\n                15m             ; Retry\n                30d             ; Expire\n                2h              ; Negative Cache TTL\n        )\n        NS  localhost."
+    print(header)
+    return header
+
+def salvar_resultados_no_arquivo(dados):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    nome_arquivo = f"dns_entries_{timestamp}.txt"
+    with open(nome_arquivo, 'w') as file:
+        file.write(dados)
+    print(f"Resultado salvo em {nome_arquivo}")
+
+def salvar_to_export(dados):
+    nome_arquivo = "dns_entries.txt"
+    with open(nome_arquivo, 'w') as file:
+        file.write(dados)
+    print(f"Resultado salvo em {nome_arquivo}")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Uso: python3 script.py <caminho_do_arquivo>")
+        print("Uso: python3 script.py <caminho_do_PDF>")
         sys.exit(1)
 
-    caminho_do_arquivo = sys.argv[1]
-    texto_extraido = extrair_texto_de_pdf(caminho_do_arquivo)
+    caminho_pdf = sys.argv[1]
+    texto_extraido = extrair_texto_de_pdf(caminho_pdf)
 
-    # Expressão regular para encontrar URLs
     regex_urls = r"\b(?:http(?:s)?://)?\w+[\w\-\.]+(?:\.\w+)+\b"
     urls_encontradas = re.findall(regex_urls, texto_extraido)
-
-    # Filtrar URLs que contêm 'policia' ou '.gov'
     urls_filtradas = [url for url in urls_encontradas if not any(proibido in url.lower() for proibido in ["policia", ".gov"])]
 
-    # Gerar e imprimir o cabeçalho DNS
-    print(gerar_cabecalho_dns())
+    cabecalho_dns = imprimir_cabecalho_dns()
+    resultado_dns = cabecalho_dns + "\n"
 
-    # Gerar e imprimir as entradas DNS para cada URL filtrada
     for url in urls_filtradas:
         entradas_dns = gerar_entradas_dns(url)
         print(entradas_dns)
+        resultado_dns += entradas_dns + "\n"
+
+    # Salvar o resultado em um arquivo com timestamp
+    salvar_resultados_no_arquivo(resultado_dns)
+    salvar_to_export(resultado_dns)
